37b67922a9914ff1d495a50e99c2ab75a2b80114
JC <jingdec2@illinois.edu>
2020-05-22 15:08:56 -0500

    add LD path to rc

--- setup/setup.sh
+++ setup/setup.sh
@@ -38,4 +38,5 @@ jenv_setup() {
     for path in "${JENV_PATH[@]}"; do 
         check_append "export PATH=$path:\$PATH" $JENV_RC
     done
+    check_append "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$LIB" $JENV_RC
 }
