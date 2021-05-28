14c9e675545014b25664274fc22c9bd0c889d423
JC <chenjd98@163.com>
2021-05-28 15:39:08 -0400

    tmp

--- src/jget/formula/justenv.sh
+++ src/jget/formula/justenv.sh
@@ -1,10 +1,10 @@
 
 get() {
-    [[ -n $JUSTENV_SRC ]] || { MSG+=("Error in installing justenv. JUSTENV_SRC not set."); return 1; }
-    cp $JUSTENV_SRC/justenv $BIN
+    [[ -n $JUSTENV_SRC ]] || { git clone https://github.com/Keitokuch/justenv.git ; JUSTENV_SRC=justenv/src ; }
+    cp -f $JUSTENV_SRC/justenv $BIN
     cp -rf $JUSTENV_SRC/jget $BIN
 }
 
 exists() {
-    [[ -x $(command -v justenv) ]]
+    has_executable justenv
 }
