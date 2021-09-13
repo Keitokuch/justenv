476eccce6ced6d4fd47e9ef88e785aabd62d105c
JC <chenjd98@163.com>
2021-09-13 17:32:12 -0400

    Add gdb. not passing mac -b

---
+++ src/jget/formula/gdb.sh
@@ -0,0 +1,16 @@
+GDB_VERSION=9.2
+
+get() {
+    version=${version:-$GDB_VERSION}
+    wget http://ftp.gnu.org/gnu/gdb/gdb-$version.tar.xz || return
+    tar xvf gdb-$version.tar.xz
+    cd gdb-$version
+    mkdir gdbbuild && cd gdbbuild
+    ../configure --prefix=$JGET  || return
+    make -j $nr_worker          || return
+    make install                || return
+}
+
+exists() {
+    [[ -x $(command -v gdb) ]]
+}
